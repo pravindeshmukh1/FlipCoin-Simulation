2f88e34dce77ed99ffda5595d00c0096ba8a7dde
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-06 14:41:19 +0530

    ADDED: Display Heads or Tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
-echo "Welcome Flip Coin Simulator"
+echo "Welcome Flip Coin Simulator UC 1"
+
+coinFlip=$((RANDOM%2))
+if [ $coinFlip -eq 0 ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
+echo $coinFlip
