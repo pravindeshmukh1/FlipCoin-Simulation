579869e18af3b01f1671b7c5aa56caa91c2cb274
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-06 17:52:42 +0530

    ADDED : Percentage of the Singlet Combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,32 @@
 #!/bin/bash -x
+declare -A coinDict
 
-echo "Welcome Flip Coin Simulator UC 1"
+echo "Welcome Flip Coin Simulator"
 
-coinFlip=$((RANDOM%2))
-if [ $coinFlip -eq 0 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
-echo $coinFlip
+read -p "Enter the number of Flipping Coin" noOfFlip
+choice=1
+
+		for ((i=1; i<=$noOfFlip; i++))
+		do
+			for ((j=1; j<=$choice; j++))
+			do
+	        ht=""
+                        checkCoin=$((RANDOM%2))
+                        if [ $checkCoin -eq 0 ]
+                        then
+                                ht+="Heads"
+                        else
+                                ht+="Tails"
+                        fi
+			done
+	coinDict[$ht]=$((${coinDict[$ht]}+1))
+	done
+
+echo "-key-" ${!coinDict[@]}
+echo "-value-" ${coinDict[@]}
+ for  i in ${!coinDict[@]}
+	do
+		coinDict[$i]=$((${coinDict[$i]}*100/$noOfFlip))
+	done
+echo "Percentage of Singlet Combination" ${coinDict[@]}
+
