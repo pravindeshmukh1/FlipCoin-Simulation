daaad6696ee37140cf9e2f1308bd683c68f9cbd5
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-07 13:12:50 +0530

    ADDED: double_simulator

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,19 +3,16 @@ declare -A coinDict
 
 echo "Welcome Flip Coin Simulator"
 
-read -p "Enter the number of Flipping Coin" noOfFlip
-echo "/n1.single /n2.double /n3.thiree"
-read choice
-filpCoin $noOfFlip $choice
-
-
 function filpCoin() {
+declare -A coinDict
+	noOfFlip=$1
+	choice=$2
 	for ((i=1; i<=$noOfFlip; i++))
 	do
+		ht=""
 		for ((j=1; j<=$choice; j++))
 		do
-		ht=""
-		checkCoin=$((RANDOM%2))
+			checkCoin=$((RANDOM%2))
 	      if [ $checkCoin -eq 0 ]
    	   then
 				ht+="H"
@@ -23,30 +20,28 @@ function filpCoin() {
 				ht+="T"
 			fi
 		done
-		#coinDict[$ht]=$((${coinDict[$ht]}+1))
-		coinDt $ht
+	coinDict[$ht]=$((${coinDict[$ht]}+1))
 	done
 percentage
 }
-echo "-key-" ${!coinDict[@]}
-echo "-value-" ${coinDict[@]}
 
-function coinDt() {
-		coinDict[$ht]=$((${coinDict[$ht]}+1))
-echo "-key-" ${!coinDict[@]}
-echo "-value-" ${coinDict[@]}
-
-}
 function percentage() {
 for  i in ${!coinDict[@]}
 do
-#	coinDict[$i]=$((${coinDict[$i]}/$noOfFlip*100))
-	echo "coinDict[$i]=$(echo "scale=2;${coinDict[$i]}/$noOfFlip*100" | bc )"
+	coinDict[$i]=$(echo "scale=2;${coinDict[$i]}/$noOfFlip*100" | bc )
 done
+	echo "Percentage of Singlet Combination" ${coinDict[@]}
 }
-echo "Percentage of Singlet Combination" ${coinDict[@]}
 
+read -p "do you want to flip coin Y/N : " input
 
-echo "-key-" ${!coinDict[@]}
-echo "-value-" ${coinDict[@]}
-
+	while [[ $input -eq "Y" && $input -eq "y" ]]
+	do
+		read -p "Enter the number of flip : " noOfFlip
+		echo -e "\n1.single \n2.double"
+		read -p "Enter your Choice : " choice
+		if [[ $choice -eq 1 || $choice -eq 2  ]]
+		then
+			filpCoin $noOfFlip $choice
+		fi
+	done
